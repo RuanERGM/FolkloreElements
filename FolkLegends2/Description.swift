9767e2c9665b68371ef4af166f280e9503e85cde
Luciano Uchoa <lucianou54@gmail.com>
2022-06-04 19:28:35 -0400

    Colocar os assets dos personagens na tela de conversa

--- FolkLegends2/Description.swift
+++ FolkLegends2/Description.swift
@@ -14,19 +14,23 @@ class Description: UIViewController{
         if (x == 1){
             Descricao.text = "Eu sou aquele nascido da terra, aquele que tem os olhos espalhados por esta terra! Meu nome é Guaraná. Agora me enfrente!"
             name.text = "Guaraná"
+            foto.image = UIImage(named: "Guarana")
         }
         
         if (x == 2){
             Descricao.text = "Eu sou aquele que foi traído e transformado em anta por aqueles em quem confiei. Birá é meu nome, vamos lutar!"
             name.text = "Birá"
+            foto.image = UIImage(named: "Peixes")
         }
         if (x == 3){
             Descricao.text = "As pessoas tremem enquanto tentam pronunciar meu nome. Eu sou aquele que vagueia nas profundezas dos sonhos.Se você tiver coragem,tente me vencer..."
             name.text = "Tutu"
+            foto.image = UIImage(named: "Tutu")
         }
         if (x == 4){
             Descricao.text = "Eu sou o senhor das chamas, minhas asas guardam os poderes do fogo. Qualquer um que tentar tomar esse poder terá que me enfrentar!"
             name.text = "Urubu-Rei"
+            foto.image = UIImage(named: "Urubu")
         }
     
     
