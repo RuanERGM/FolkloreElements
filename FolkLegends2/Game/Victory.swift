ef0b7602d74c13d33f9fb0cd851d920827a39587
Anna Milani <aavm.snf19@uea.edu.br>
2022-06-05 11:08:42 -0400

    Adicionar referencia da carta do bestiario na tela de vitoria

--- FolkLegends2/Game/Victory.swift
+++ FolkLegends2/Game/Victory.swift
@@ -2,7 +2,26 @@ import UIKit
 
 class Victory: UIViewController{
     
+    @IBOutlet weak var cardUnloked: UIImageView!
+    
+    func getCardUnloked() -> String {
+        switch UserKeys.allDoor {
+        case 1:
+            return "card.guarana"
+        case 2:
+            return "card.peixe"
+        case 3:
+            return "card.tutu"
+        case 4:
+            return "card.bahira"
+        default:
+            return "bes.card.back"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cardUnloked.image = UIImage(named: getCardUnloked())
+        
     }
 }
