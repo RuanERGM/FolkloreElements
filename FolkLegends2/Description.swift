ee3affb73bd6587409062297d7d7d3f71fc30341
Ruan Eleuterio Rodrigues Gois Magalhães <ruaneleuterio161@gmail.com>
2022-06-10 09:05:03 -0400

    Pre-jogo responsivo

--- FolkLegends2/Description.swift
+++ FolkLegends2/Description.swift
@@ -4,9 +4,10 @@ import Foundation
 class Description: UIViewController{
   
 
-    @IBOutlet weak var name: UILabel!
+   
     @IBOutlet weak var foto: UIImageView!
     @IBOutlet weak var Descricao: UILabel!
+    @IBOutlet weak var Name: UIImageView!
     
     @IBOutlet var Description: UIView!
     override func viewDidLoad() {
@@ -14,23 +15,23 @@ class Description: UIViewController{
         
         if (x == 1){
             Descricao.text = "Eu sou aquele nascido da terra, aquele que tem os olhos espalhados por ela toda! Meu nome é Guaraná. Agora me enfrente!"
-            name.text = "Guaraná"
+            Name.image = UIImage(named: "Guarana.name")
             foto.image = UIImage(named: "Guarana")
         }
         
         if (x == 2){
             Descricao.text = "Eu sou aquele que foi traído e transformado em anta por aqueles em quem confiei. Birá é meu nome, vamos lutar!"
-            name.text = "Birá"
+            Name.image = UIImage(named: "SurgPei.name")
             foto.image = UIImage(named: "Bira")
         }
         if (x == 3){
             Descricao.text = "As pessoas tremem enquanto tentam pronunciar meu nome. Eu sou aquele que vagueia nas profundezas dos seus sonhos.Se você tiver coragem, tente me vencer..."
-            name.text = "Tutu"
+            Name.image = UIImage(named: "tutu.name")
             foto.image = UIImage(named: "Tutu")
         }
         if (x == 4){
             Descricao.text = "Eu sou o senhor das chamas, minhas asas guardam os poderes do fogo. Qualquer um que tentar tomar esse poder terá que me enfrentar!"
-            name.text = "Urubu-Rei"
+            Name.image = UIImage(named: "Urubu-rei.name")
             foto.image = UIImage(named: "Urubu")
         }
     
